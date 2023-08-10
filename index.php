2782154cd6104bc64222be0b53b6d26cfb0b4ebf
JBxss <juanseb100@gmail.com>
2023-08-10 00:03:17 -0500

    [UPDATE] Comentarios añadidos

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+// Incluye las librerías y el archivo de la vista de la aplicación (contiene lógica relacionada con la interfaz)
 require 'vendor/autoload.php';
 require 'src/views/app.php';
 
@@ -19,18 +20,22 @@ require 'src/views/app.php';
 <body>
     <main>
 
+        <!-- Muestra la imagen del Pokémon obtenida a través de la función arrayPokemon() -->
         <img src="<?php echo arrayPokemon()['sprites']['front_default']; ?>" alt="pokemon" class="pokemon__image">
 
+        <!-- Muestra el número y el nombre del Pokémon obtenidos a través de la función arrayPokemon() -->
         <h1 class="pokemon__data">
             <span class="pokemon__number"><?php echo arrayPokemon()['id']; ?></span> -
             <span class="pokemon__name"><?php echo arrayPokemon()['name']; ?></span>
         </h1>
 
+        <!-- Formulario de búsqueda de Pokémon por nombre o número -->
         <form class="form" method="post">
             <input type="search" class="input__search" placeholder="Name or Number" name="search" required />
             <h4 style="padding-top: 10px;">01 - 1010</h4>
         </form>
 
+        <!-- Formulario con botones para navegar entre Pokémon (anterior y siguiente) -->
         <form method="post">
             <div class="buttons">
                 <button class="button btn-prev" name="prev">&lt; Prev</button>
@@ -38,6 +43,7 @@ require 'src/views/app.php';
             </div>
         </form>
 
+        <!-- Muestra una imagen de la Pokédex al final de la página -->
         <img src="src/img/pokedex.png" alt="pokedex" class="pokedex">
     </main>
 </body>
--- src/views/app.php
+++ src/views/app.php
@@ -1,10 +1,12 @@
 <?php
 session_start();
 
+// Función para obtener el ID del Pokémon, ya sea a través del formulario o navegación
 function obtenerID()
 {
-
+    // ID predeterminada inicial
     $idPredeterminada = 1;
+    // Obtiene el valor del formulario de búsqueda por POST
     $idFromForm = $_POST['search'];
 
     if (isset($_POST['prev'])) {
@@ -21,30 +23,36 @@ function obtenerID()
     if (!empty($idFromForm)) {
         return $idFromForm;
     } else {
+        // Si no, devuelve la ID predeterminada
         return $idPredeterminada;
     }
 }
 
+// Función para obtener los datos del Pokémon utilizando la API PokeAPI
 function arrayPokemon()
 {
     // Inicializa una sesion de cURL
     $ch = curl_init();
     $url = "https://pokeapi.co/api/v2/pokemon/";
+    
+    // Obtiene el ID del Pokémon utilizando la función obtenerID()
     $pokemon = $url . obtenerID();
     curl_setopt($ch, CURLOPT_URL, $pokemon);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
+    // Realiza la solicitud cURL
     $response = curl_exec($ch);
 
+    // Manejo de errores cURL
     if (curl_errno($ch)) {
         $errormsg = curl_error($ch);
         echo "Error al conectarse a la API " . $errormsg;
     } else {
         curl_close($ch);
 
-        // Decode JSON data into PHP array
+        // Decodifica los datos JSON en un array PHP
         $pokemon_data = json_decode($response, true);
-        // Store all pokemon results in a variable
+        // Retorna los datos del Pokémon
         return $pokemon_data;
     }
 }
